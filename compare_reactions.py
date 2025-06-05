6281bebbce26a08f155eaccf28e4cc66a08eb7ac
reeshapatel12 <reesha.patel387@gmail.com>
2025-06-05 16:05:07 -0400

    updated script to compare reactions and return changes

--- compare_reactions.py
+++ compare_reactions.py
@@ -1,5 +1,3 @@
-# compare the reactant and products, note the change
-
 # define changes
 state_change_up = "change from bottom state to top state"
 state_change_down = "change from top state to bottom state"
@@ -9,8 +7,7 @@ bond_removed_non_rev = "nrbroken"
 bond_added_rev = "radded"
 bond_removed_rev = "rbroken"
 
-def compare_reactions(expanded_reactants, expanded_products, arrow):
-    stripped_changes = {}
+def compare_reactions(expanded_reactants, expanded_products, arrow, mol_site_dict):
     changes_dict = {}
 
     reactant_parts = expanded_reactants.split(".")
@@ -18,105 +15,79 @@ def compare_reactions(expanded_reactants, expanded_products, arrow):
 
     all_rsites = []
     for part in reactant_parts:
+        rmol = part.split("(")[0]
         rsites = part.split("(")[1].rstrip(")")
         rsites_parts = rsites.split(",")
-        all_rsites+=rsites_parts
+        for site in rsites_parts:
+            all_rsites.append((rmol, site))
     
     all_psites = []
     for part in product_parts:
+        pmol = part.split("(")[0]
         psites = part.split("(")[1].rstrip(")")
         psites_parts = psites.split(",")
-        all_psites+=psites_parts
+        for site in psites_parts:
+            all_psites.append((pmol, site))
     
     for i in range(len(all_rsites)):
-        if all_rsites[i] != all_psites[i]:
-            changes_dict[all_rsites[i]] = all_psites[i]
-    
-    for key,value in changes_dict.items():
-        if "~" in key:
-            rstate = "~"+key.split("~",-1)[-1]
-            if "!" in rstate:
-                rstate = rstate.split("!")[0]
-            stripped_changes[rstate] = ""
-        else:
-            rbond = "!"+key.split("!")[-1]
-            stripped_changes[rbond] = ""
-        
-        if "~" in value:
-            pstate = "~"+value.split("~",-1)[-1]
-            if "!" in pstate:
-                pstate = pstate.split("!")[0]
-            stripped_changes[rstate] = pstate
-        else:
-            pbond = "!"+value.split("!")[-1]
-            stripped_changes[rbond] = pbond
+        rmol,r_raw = all_rsites[i]
+        pmol,p_raw = all_psites[i]
 
-        print(stripped_changes)
-    
-
+        if r_raw != p_raw:
+            rstate = pstate = None
+            rsite = psite = None
+            rbond = pbond = None
+            change = ""
+            if "~" in r_raw:
+                rstate = "~"+r_raw.rsplit("~",1)[-1]
+                rsite = r_raw.split("~")[0]
+                if "!" in rstate:
+                    rstate = rstate.split("!")[0]
+            else:
+                rbond = "!"+r_raw.split("!")[-1]
+                rsite = r_raw.split("!")[0]
             
+            if "~" in p_raw:
+                pstate = "~"+p_raw.rsplit("~",1)[-1]
+                psite = p_raw.split("~")[0]
+                if "!" in pstate:
+                    pstate = pstate.split("!")[0]
+            else:
+                pbond = "!"+p_raw.split("!")[-1]
+                psite = p_raw.split("!")[0]   
+
+            if rstate != pstate:
+                mole_sites = mol_site_dict[rmol]
+                for site in mole_sites:
+                    if site.startswith(rsite+"~"):
+                        state_list = site.split("~")[1:]
+                        if state_list.index(rstate.split("~")[1]) <  state_list.index(pstate.split("~")[1]):
+                            change = state_change_down
+                        elif state_list.index(rstate.split("~")[1]) >  state_list.index(pstate.split("~")[1]):
+                            change = state_change_up
+
+
+            elif rbond != pbond:
+                if arrow == "->":
+                    if rbond == "!-":
+                        change = bond_added_non_rev
+                    else:
+                        change = bond_removed_non_rev
+                else:
+                    if rbond == "!-":
+                        change = bond_added_rev
+                    else:
+                        change = bond_removed_rev
+
+            changes_dict[f'{rmol}:{rsite}'] = {
+                "molecule" : rmol,
+                "site" : rsite,
+                "reactant" : r_raw,
+                "product" : p_raw,
+                "change" : change
+            }
     
-
-
-#test
-compare_reactions("egfr(l!-,r!-,Y1068~Y~pY!?,Y1148~Y~pY!?).egf(r!-)", 
-                  "egfr(l!1,r!-,Y1068~Y~pY!?,Y1148~Y~pY!?).egf(r!1)",
-                  "<->" 
-)
-
-compare_reactions("egfr(l!+,r!-,Y1068~Y~pY!?,Y1148~Y~pY!?).egfr(l!+,r!-,Y1068~Y~pY!?,Y1148~Y~pY!?)", 
-                  "egfr(l!+,r!3,Y1068~Y~pY!?,Y1148~Y~pY!?).egfr(l!+,r!3,Y1068~Y~pY!?,Y1148~Y~pY!?)",
-                  "<->" 
-)
-
-compare_reactions("egfr(l!+,r!-,Y1068~Y!?,Y1148~Y~pY!?).egfr(l!+,r!-,Y1068~Y~pY!?,Y1148~Y~pY!?)", 
-                  "egfr(l!+,r!3,Y1068~pY!?,Y1148~Y~pY!?).egfr(l!+,r!3,Y1068~Y~pY!?,Y1148~Y~pY!?)",
-                  "<->" 
-)
-
-compare_reactions("egfr(l!?, r!+,Y1068~Y!-, Y1148~Y~pY!?)", 
-                  "egfr(l!?, r!+,Y1068~pY!-, Y1148~Y~pY!?)", 
-                  "->"
-)
-
-compare_reactions("egfr(l!?, r!+,Y1068~Y~pY!?, Y1148~Y!-)",
-                  "egfr(l!?, r!+,Y1068~Y~pY!?, Y1148~pY!-)",
-                  "->" 
-)
-
-compare_reactions("egfr(l!?, r!+,Y1068~pY!-, Y1148~Y~pY!?)", 
-                  "egfr(l!?, r!+,Y1068~Y!-, Y1148~Y~pY!?)",
-                  "->" 
-)
-
-compare_reactions("egfr(l!?, r!+,Y1068~Y~pY!?, Y1148~pY!-)",
-                  "egfr(l!?, r!+,Y1068~Y~pY!?, Y1148~Y!-)", 
-                  "->" 
-)
-
-compare_reactions("egfr(l!?,r!+,Y1068~Y~pY!?,Y1148~pY!1).Shc(PTB!1,Y317~Y!-)", 
-                  "egfr(l!?,r!+,Y1068~Y~pY!?,Y1148~pY!1).Shc(PTB!1,Y317~pY!-)",
-                  "->" 
-)
-
-compare_reactions("Shc(PTB!+,Y317~pY!-)", 
-                  "Shc(PTB!+,Y317~Y!-)",
-                  "->" 
-)
-
-
-
-    
-
-
-
-# change in state - take the original molecule, and apply the multiple choice 
-
-# up-arrow if change from bottom state to top state
-
-# down-arrow if change from top state to bottom state
-
-
+    return changes_dict
 
 # change in bonds - take molecule with bond number (either reactant or product)
 
